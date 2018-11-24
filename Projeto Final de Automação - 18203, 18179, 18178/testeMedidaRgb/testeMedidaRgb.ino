516d66e8267db1494cfbae4c140ed9415dc33dea
Scherer <fsvicentin@hotmail.com>
2018-11-24 18:19:24 -0200

    Adicao do micro servo no codigo

--- Projeto Final de Automação - 18203, 18179, 18178/testeMedidaRgb/testeMedidaRgb.ino
+++ Projeto Final de Automação - 18203, 18179, 18178/testeMedidaRgb/testeMedidaRgb.ino
@@ -1,9 +1,21 @@
+#include <SoftwareSerial.h>
+#include <Stepper.h>
+#include <Servo.h>
+
 #define ldr A0
+#define rx 3
+#define tx 2
+#define pServo 6
+#define graus 10
+
+Servo servo;
+Stepper motor(360/graus, 8, 10, 9, 11);
+SoftwareSerial bt(rx, tx);
 
 //Matriz de floats para armazenar o valor das cores 
-float rgb[] = {   0,0,0}; 
-float matrizBranco[] = {   0,0,0}; 
-float matrizPreto[] = {   0,0,0}; 
+float rgb[] = {0,0,0};
+float matrizBranco[] = {0,0,0}; 
+float matrizPreto[] = {0,0,0}; 
 
 //Variável para armazenar a média das leitura 
 int mediaLeituras;
@@ -14,11 +26,13 @@ void setup() {
   for (int i = 0; i < 3; i++)
     pinMode(rgbLed[i], OUTPUT);
   pinMode(ldr, INPUT);
+  servo.attach(pServo);
   Serial.begin(9600);
+  servo.write(120);
 }
 
 void loop() {
-  if (!jaCalibrou)
+  if (jaCalibrou)
   {
     //Calibrando o branco!   
     Serial.println("Calibrando o branco");   
@@ -70,7 +84,10 @@ void loop() {
   teste.concat(rgb[2]);
   teste.concat(")");
   Serial.println(teste);
-  //delay(200);
+  servo.write(90);
+  delay(1500);
+  servo.write(0);
+  delay(300);
 }
 
 void mediaSensor(int n)
