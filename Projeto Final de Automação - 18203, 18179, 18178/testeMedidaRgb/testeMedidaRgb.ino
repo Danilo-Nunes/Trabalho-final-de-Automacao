fd4192669493ddb6c57569027be4e36d4293b8cd
Scherer <fsvicentin@hotmail.com>
2018-12-02 17:26:38 -0200

    avanco no codigo

--- Projeto Final de Automação - 18203, 18179, 18178/testeMedidaRgb/testeMedidaRgb.ino
+++ Projeto Final de Automação - 18203, 18179, 18178/testeMedidaRgb/testeMedidaRgb.ino
@@ -12,26 +12,64 @@ Servo servo;
 Stepper motor(360/graus, 8, 10, 9, 11);
 SoftwareSerial bt(rx, tx);
 
-//Matriz de floats para armazenar o valor das cores 
+//Matriz de floats para armazenar o valor das cores e a angulacao correspondente
 float rgb[] = {0,0,0};
 float matrizBranco[] = {0,0,0}; 
-float matrizPreto[] = {0,0,0}; 
+float matrizPreto[] = {0,0,0};
+int angulacoes[] = {0, 0, 0, 0, 0, 0};
 
-//Variável para armazenar a média das leitura 
+//Variável para armazenar a média das leitura e da cor do m&m atual
 int mediaLeituras;
+int corAtual;
+int estado;
 boolean jaCalibrou = false;
-
 int rgbLed[] = {12, 13, 7};
+
 void setup() {
   for (int i = 0; i < 3; i++)
     pinMode(rgbLed[i], OUTPUT);
   pinMode(ldr, INPUT);
+  motor.setSpeed(50);
   servo.attach(pServo);
   Serial.begin(9600);
   servo.write(120);
+  bt.begin(38400);
 }
 
 void loop() {
+  if (bt.available() > 0)
+  {
+    estado = bt.readString().toInt();
+  }
+  if (estado == 1)
+  {
+    lerValores();
+  }
+  if (estado == 2)
+  {
+    while((bt.available() > 0)&&(estado == 2))
+      estado = bt.readString().toInt();
+  }
+  if (estado == 3)
+  {
+    return;
+  }
+}
+
+void mediaSensor(int n)
+{
+  int val;
+  int soma;
+  for (int i = 0; i < n; i++)
+  {
+    val = analogRead(ldr);
+    soma += val;
+    delay(10);
+  }
+  mediaLeituras = soma/n;
+}
+void lerValores()
+{
   if (jaCalibrou)
   {
     //Calibrando o branco!   
@@ -76,29 +114,62 @@ void loop() {
     digitalWrite(rgbLed[i], LOW);
     delay(100);
   }
-  String teste = "rgb(";
-  teste.concat(rgb[0]);
-  teste.concat(", ");
-  teste.concat(rgb[1]);
-  teste.concat(", ");
-  teste.concat(rgb[2]);
-  teste.concat(")");
-  Serial.println(teste);
-  servo.write(90);
-  delay(1500);
-  servo.write(0);
-  delay(300);
-}
-
-void mediaSensor(int n)
-{
-  int val;
-  int soma;
-  for (int i = 0; i < n; i++)
+  //rgb[0] - red
+  //rgb[1] - green
+  //rgb[2] - blue
+  if (rgb[0] > rgb[1] && rgb[0] > rgb[2]) //se o vermelhor for o maior de todos, a cor do m&m pode ser: vermelho, laranja, amarelo ou marrom
   {
-    val = analogRead(ldr);
-    soma += val;
-    delay(10);
+    if (rgb[0] < 100) //se o vermelho for muito baixo, o m&m é marrom
+    {
+      //marrom
+      corAtual = 4;
+    }
+    else if (rgb[0] - rgb[1] < 50) //se a diferenca entre o vermelho e o verde for pequena, é amarelo
+    {
+      //amarelo
+      corAtual = 2;
+    }
+    else if (rgb[0] - rgb[1]*2 < 50) //se o verde for mais ou menos metade do vermelho, é laranja
+    {
+      //laranja
+      corAtual = 3;
+    }
+    else //se o verde for muito pequeno, é vermelho
+    {
+      //vermelho
+      corAtual = 6;
+    }
   }
-  mediaLeituras = soma/n;
+  else if (rgb[1] > rgb[0] && rgb[1] > rgb[2]) // se o verde for o maior, o m&m é verde
+  {
+    //verde
+    corAtual = 5;
+  }
+  else //se o azul for o maior, o m&m é azul
+  {
+    //azul
+    corAtual = 1;
+  }
+  motor.step(angulacoes[corAtual-1]); //gira o motor de passo com a angulacao desejada de acordo com a cor do m&m
+  delay(1000);
+  //faz a rampa cair
+  //rampa sobe de novo
+  motor.step(360-angulacoes[corAtual-1]); //motor de passo volta a posicao inicial
+  delay(500);
+  String aux = corAtual + "";
+  bt.println(aux);
+  bt.flush();
 }
+//void setupBlueToothConnection()
+//{
+//  bt.begin(9600);
+//  bt.print("\r\n+STWMOD=0\r\n"); //set the bluetooth work in slave mode
+//  bt.print("\r\n+STNA=HC-05\r\n"); //set the bluetooth name as "SeeedBTSlave"
+//  bt.print("\r\n+STOAUT=1\r\n"); // Permit Paired device to connect me
+//  bt.print("\r\n+STAUTO=0\r\n"); // Auto-connection should be forbidden here
+//  delay(2000); // This delay is required.
+//  bt.print("\r\n+INQ=1\r\n"); //make the slave bluetooth inquirable 
+//  Serial.println("The slave bluetooth is inquirable!");
+//  delay(2000); // This delay is required.
+//  bt.flush();
+//}
